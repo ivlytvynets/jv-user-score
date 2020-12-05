e3da9e8f38a365a2259436daa1dce3982fcc7e4b
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-05 20:58:13 +0200

    3 test failed, 1 completed

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -2,29 +2,16 @@ package core.basesyntax;
 
 import exception.UserNotFoundException;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class UserService {
 
     public int getUserScore(String[] records, String email) throws UserNotFoundException {
-        String[][] data = new String[records.length][2];
-        for (int i = 0; i < records.length; i++) {
-            data[i][0] = records[i].split(":")[0];
-            data[i][1] = records[i].split(":")[1];
-        }
-        int indexEmail = 0;
-        boolean isContainEmail = false;
-        for (int i = 0; i < records.length && !isContainEmail; i++) {
-            if (data[i][0] == email) {
-                isContainEmail = true;
-                indexEmail = i;
+        for (String record:
+                records) {
+            String[] data = record.split(":");
+            if (data[0].equals(email)) {
+                return Integer.parseInt(data[1]);
             }
         }
-        if (!isContainEmail) {
-            throw new UserNotFoundException("User with given email doesn't exist");
-        }
-
-        return Integer.parseInt(data[indexEmail][1]);
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
--- src/main/java/exception/UserNotFoundException.java
+++ src/main/java/exception/UserNotFoundException.java
@@ -1,6 +1,7 @@
 package exception;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String s) {
+        super(s);
     }
 }
